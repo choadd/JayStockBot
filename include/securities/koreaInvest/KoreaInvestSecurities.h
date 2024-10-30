a5a819af4ebc4b20faea0a1debebf045ab7c802a
choadd <choadd@naver.com>
2024-10-30 13:09:01 +0000

    [securities/koreaInvest] add stock lookup API

--- include/securities/koreaInvest/KoreaInvestSecurities.h
+++ include/securities/koreaInvest/KoreaInvestSecurities.h
@@ -2,21 +2,26 @@
 #define KOREA_INVEST_SECURITIES_H
 
 #include <string>
+#include "base/thread/Thread.h"
 #include "securities/SecuritiesInterface.h" // SecuritiesInterface 포함
 
-class KoreaInvestSecurities {
+class KoreaInvestSecurities : public SecuritiesInterface, public Thread {
 public:
     // 생성자
     KoreaInvestSecurities(const SecurityConfig& config);
     ~KoreaInvestSecurities();
 
     // 메서드 오버라이드
-    std::string getAccessToken() ;
-    std::string getHashkey(const std::string& data) ;
-    bool loadConfig(const std::string& configPath) ;
+    std::string getAccessToken();
+    std::string getHashkey(const std::string& data);
+    bool loadConfig(const std::string& configPath);
+    int getCurrentPrice(const std::string& code);
+
+    void mainLoop();
 
 protected:
     SecurityConfig securityConfig; // SecurityConfig 객체
+    int tId;
 };
 
 #endif // KOREA_INVEST_SECURITIES_H
--- src/securities/koreaInvest/KoreaInvestSecurities.cpp
+++ src/securities/koreaInvest/KoreaInvestSecurities.cpp
@@ -3,8 +3,19 @@
 #include <yaml-cpp/yaml.h>
 #include <curl/curl.h> // libcurl 헤더
 #include <string.h>
+#include <json/json.h>
 
 #include "securities/koreaInvest/KoreaInvestSecurities.h"
+
+// 생성자 구현
+KoreaInvestSecurities::KoreaInvestSecurities(const SecurityConfig& config) {
+    securityConfig = config; // securityConfig 초기화
+}
+
+KoreaInvestSecurities::~KoreaInvestSecurities() {
+    // 자원 해제 및 필요한 클린업 코드 추가
+    // 예: 동적으로 할당된 메모리 해제 등
+}
 
 // CURL 응답을 저장하기 위한 구조체
 struct MemoryStruct {
@@ -24,16 +35,6 @@ static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, Mem
     userp->size += realsize;
     userp->memory[userp->size] = 0; // null-terminate the string
     return realsize;
-}
-
-// 생성자 구현
-KoreaInvestSecurities::KoreaInvestSecurities(const SecurityConfig& config) {
-    securityConfig = config; // securityConfig 초기화
-}
-
-KoreaInvestSecurities::~KoreaInvestSecurities() {
-    // 자원 해제 및 필요한 클린업 코드 추가
-    // 예: 동적으로 할당된 메모리 해제 등
 }
 
 // config.yaml 파일에서 설정 읽기
@@ -92,6 +93,7 @@ std::string KoreaInvestSecurities::getAccessToken() {
             pos += 16; // "access_token": 의 길이
             size_t endPos = response.find("\"", pos);
             accessToken = response.substr(pos, endPos - pos);
+            securityConfig.token = accessToken;
             std::cout << "Access Token: " << accessToken << std::endl; // 출력 추가
         }
 
@@ -158,4 +160,98 @@ std::string KoreaInvestSecurities::getHashkey(const std::string& data) {
     }
 
     return "";
+}
+
+int KoreaInvestSecurities::getCurrentPrice(const std::string& code) {
+    const std::string PATH = "uapi/domestic-stock/v1/quotations/inquire-price";
+    const std::string URL = securityConfig.urlBase + "/" + PATH;
+
+    // Debug 정보 출력
+    // std::cout << "SecurityConfig:" << std::endl;
+    // std::cout << "  App Key: " << securityConfig.appKey << std::endl;
+    // std::cout << "  App Secret: " << securityConfig.appSecret << std::endl;
+    // std::cout << "  URL Base: " << securityConfig.urlBase << std::endl;
+    // std::cout << "  Token: " << securityConfig.token << std::endl;
+
+    // 요청 헤더 설정
+    struct curl_slist* headers = nullptr;
+    std::string token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjY2NmM5MjFkLWU4ZGItNDg0YS04NmEyLWU5MDMwYTZmYTY4YiIsInByZHRfY2QiOiIiLCJpc3MiOiJ1bm9ndyIsImV4cCI6MTczMDM3NjY0MSwiaWF0IjoxNzMwMjkwMjQxLCJqdGkiOiJQU0hweWZBeUlTcWhKaXI4cnpxUzdNdldtaElZMWNtUjhNU2wifQ.PCRquF-Hzv0412tWC-kj6C47dWa0fmhtob6eGY6xuYOHQBiALIMaMZOn-BBn_FPFdQ1sijM2l46t921k3HJEYA";
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+    headers = curl_slist_append(headers, ("authorization: Bearer " + token).c_str());
+    headers = curl_slist_append(headers, ("appKey: " + securityConfig.appKey).c_str());
+    headers = curl_slist_append(headers, ("appSecret: " + securityConfig.appSecret).c_str());
+    headers = curl_slist_append(headers, "tr_id: FHKST01010100");
+
+    // 요청 파라미터 설정
+    std::string params = "fid_cond_mrkt_div_code=J&fid_input_iscd=" + code;
+
+    // cURL 초기화
+    CURL* curl = curl_easy_init();
+    if (!curl) {
+        std::cerr << "Failed to initialize cURL!" << std::endl;
+        return -1; // cURL 초기화 실패 시 -1 반환
+    }
+
+    CURLcode res;
+    std::string response_string;
+
+    MemoryStruct chuck;
+    chuck.memory = (char *)malloc(1); // 초기 메모리 할당
+    chuck.size = 0; // 초기 크기
+
+    // URL 설정 (GET 요청)
+    curl_easy_setopt(curl, CURLOPT_URL, (URL + "?" + params).c_str());
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    
+    // 응답 데이터를 저장하기 위한 콜백 설정
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chuck);
+
+    // 요청 실행
+    res = curl_easy_perform(curl);
+
+
+    if (res != CURLE_OK) {
+        std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+    }
+
+    std::string response(chuck.memory);
+    std::string stockPrice;
+    size_t outputPos = response.find("\"output\"");
+    if (outputPos != std::string::npos) {
+        size_t stckPrprPos = response.find("\"stck_prpr\":", outputPos);
+        if (stckPrprPos != std::string::npos) {
+            stckPrprPos += 12;  // "stck_prpr": 의 길이
+            size_t endPos = response.find("\"", stckPrprPos + 1);
+            stockPrice = response.substr(stckPrprPos + 1, endPos - stckPrprPos - 1);
+            std::cout << "Stock Price: " << stockPrice << std::endl; // 추출된 stck_prpr 값 출력
+            return 0;
+        } else {
+            std::cerr << "stck_prpr key not found in output" << std::endl;
+        }
+    } else {
+        std::cerr << "output key not found in response" << std::endl;
+    }
+
+    curl_easy_cleanup(curl);
+    curl_slist_free_all(headers);
+
+    // 오류 시 -1 반환
+    return -1;
+}
+
+
+void KoreaInvestSecurities::mainLoop(){
+    while (isRunning()) { // isRunning()은 Thread 클래스에서 정의한 메서드
+        std::cout << "KoreaInvestSecurities is running..." << std::endl; // ID 출력
+        // 필요한 작업 수행
+        getAccessToken();
+        std::this_thread::sleep_for(std::chrono::seconds(1)); // 1초 대기
+
+        std::string code = "000660";
+        std::cout << getCurrentPrice(code) << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1)); // 1초 대기
+
+        
+    }
 }
